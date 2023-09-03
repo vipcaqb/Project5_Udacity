414e0fc18bc09a93ee04ee854a792c770f1f5c5b
vipcaqb <hoangloc921999@gmail.com>
2023-09-03 21:24:57 +0700

    fix lint error

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return1 render_template('home.html')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
